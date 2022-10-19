9607c778132e3e363b3e7984651c7736e2fae302
Asonin <qin3500@icloud.com>
2022-10-19 16:59:04 +0800

    length_calc script written

--- output/post/length_calc.py
+++ output/post/length_calc.py
@@ -15,11 +15,15 @@ def video_duration_2(filename):
 dir_base = f'/home1/zhuwentao/projects/multi-camera/mvball_proj/output/wusi/'
 file = os.listdir(dir_base)
 total_length = 0
+total_14 = 0
+total_09 = 0
 sequence_calc = {}
 # 先创建并打开一个文本文件
 f = open('legth_calc.txt', 'w')
 
 for folder in file: # e.g. 1002_2
+    # print(int(folder[2]))
+    x = int(folder[2])
     p = dir_base + folder + '/'
     sequences = os.listdir(p)
     sequence_length = 0
@@ -29,6 +33,10 @@ for folder in file: # e.g. 1002_2
         if os.path.exists(p2):
             t = video_duration_2(p2)
             sequence_length += t
+            if x == 1:
+                total_14 += t
+            else:
+                total_09 += t
             total_length += t
         else:
             print(p2)
@@ -40,14 +48,20 @@ for folder in file: # e.g. 1002_2
     print(folder,sequence_calc[folder])
     f.write(str(folder)+' '+str(sequence_calc[folder])+'\n')
 
-print(total_length)
-ratio = total_length / float(17*300)
-print(ratio)
-f,write(str(total_length)+' '+str(ratio)+'\n')
 
-# 遍历字典的元素，将每项元素的key和value分拆组成字符串，注意添加分隔符和换行符
-# for k,v in sequence_calc.items():
-# 	file.write(str(k)+' '+str(v)+'\n')
+print('total_length = ',total_length)
+ratio = total_length / float(17*300)
+print('total_ratio = ',ratio)
+ratio_14 = total_14 / (float(17*300) - float(34 * 60))
+print('total_14 = ',total_14)
+print('ratio_14 = ',ratio_14)
+ratio_09 = total_09 / float(34 * 60)
+print('total_09 = ',total_09)
+print('ratio_09 = ',ratio_09)
+      
+f,write(str(total_length)+' '+str(ratio)+'\n')
+f.write(str(total_14)+' '+str(ratio_14)+'\n')
+f.write(str(total_09)+' '+str(total_09)+'\n')
 	
 # 注意关闭文件
 f.close()
